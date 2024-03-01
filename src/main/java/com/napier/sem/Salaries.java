90dda3af903a649194afb443052e739ec0017100
40667068 <158122981+40667068@users.noreply.github.com>
2024-02-29 20:47:27 -0400

    Made changes to file content

---
+++ src/main/java/com/napier/sem/Salaries.java
@@ -0,0 +1,25 @@
+package com.napier.sem;
+
+public class Salaries {
+
+    /**
+     * Employee number
+     */
+    public int emp_no;
+
+    /**
+     * Employee's first name
+     */
+    public String salary;
+
+    /**
+     * From date
+     */
+    public String from_date;
+
+    /**
+     * To date
+     */
+    public String to_date;
+}
+
---
+++ src/main/java/com/napier/sem/Titles.java
@@ -0,0 +1,20 @@
+package com.napier.sem;
+
+public class Titles {
+
+    /**
+     * Employee number
+     */
+    public int emp_no;
+
+    /**
+     * From date
+     */
+    public String from_date;
+
+    /**
+     * To date
+     */
+    public String to_date;
+}
+
